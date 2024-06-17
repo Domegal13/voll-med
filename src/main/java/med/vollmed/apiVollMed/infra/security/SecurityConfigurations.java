6019fd7cab09c97cbe29e9b6c66e16b3a1cc2a16
Domegal13 <Domegal13cursos@gmail.com>
2024-06-17 10:01:39 -0300

    Agregando Security-Filter

--- src/main/java/med/vollmed/apiVollMed/infra/security/SecurityConfigurations.java
+++ src/main/java/med/vollmed/apiVollMed/infra/security/SecurityConfigurations.java
@@ -1,7 +1,9 @@
 package med.vollmed.apiVollMed.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,9 +11,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfigurations {
+
+    @Autowired
+    private SecurityFilter securityFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -20,6 +26,11 @@ public class SecurityConfigurations {
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Configura sesiones sin estado
                 )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/login").permitAll() // Permite acceso sin autenticación a /login para métodos POST
+                        .anyRequest().authenticated() // Requiere autenticación para cualquier otra solicitud
+                )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class) // Agrega el filtro personalizado antes del filtro de autenticación
                 .build();
     }
 
